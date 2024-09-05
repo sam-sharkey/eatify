a3764d98081b2afe72aaef7861ed107df33e4001
Sam Sharkey <samsharkey.ss@gmail.com>
2024-09-05 23:21:09 +0100

    add files

---
+++ frontend/src/components/OrderPage/ProductCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    class="h-48 w-24 flex flex-col items-center justify-center p-2 text-center text-sm text-gray-700 font-inter cursor-pointer"
+    @click="addItem"
+  >
+    <div
+      class="relative flex-1 rounded-xl p-3 z-10 flex flex-col items-center justify-start"
+      :class="quantity > 0 ? 'bg-honeydew' : 'bg-antiquewhite'"
+    >
+      <!-- Item Name -->
+      <div class="w-24 py-2 px-5 text-center">
+        <div class="leading-4 overflow-hidden">
+          <span class="inline-block">{{ itemName }}</span>
+        </div>
+      </div>
+      <!-- Container Image -->
+      <div
+        class="w-full flex flex-col items-center justify-center py-2 text-xs text-white"
+      >
+        <img
+          class="w-full max-w-28 object-cover"
+          alt="container image"
+          :src="containerImage"
+        />
+        <!-- Controls: Visible only when selected -->
+        <div
+          v-if="quantity > 0"
+          class="bottom-[-0.5rem] flex flex-row items-center justify-between px-4 z-20"
+        >
+          <!-- Remove Button -->
+          <button
+            class="absolute left-4 h-8 w-8 rounded-full bg-oldlace border border-honeydew flex items-center justify-center"
+            @click.stop="removeItem"
+          >
+            <img
+              class="w-6 h-6"
+              alt="remove item"
+              src="/button--remove-spring-mix--svg.svg"
+            />
+          </button>
+          <!-- Quantity Display -->
+          <div
+            class="absolute right-4 rounded-full bg-darkslategray-200 border border-honeydew flex items-center justify-center px-2 py-1"
+            @click="addItem"
+          >
+            <span class="text-sm">{{ quantity }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "ProductCard",
+  props: {
+    itemName: { type: String, required: true }, // Name of the item
+    containerImage: { type: String, required: true }, // Container image URL
+    initialQuantity: { type: Number, default: 0 }, // Container image URL
+  },
+  setup(props) {
+    const quantity = ref(props.initialQuantity);
+
+    const addItem = () => {
+      quantity.value = quantity.value + 1;
+    };
+
+    const removeItem = () => {
+      if (quantity.value > 0) {
+        quantity.value = quantity.value - 1;
+      }
+    };
+
+    return {
+      quantity,
+      addItem,
+      removeItem,
+    };
+  },
+});
+</script>
+
+<style scoped>
+/* Additional styles can be added here if needed */
+</style>
