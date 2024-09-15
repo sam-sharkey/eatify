307c5b524d731fdd55be79c7a8b50ec313447fa3
Sam Sharkey <samsharkey.ss@gmail.com>
2024-09-15 12:50:14 +0100

    add store

---
+++ frontend/src/stores/itemStore.ts
@@ -0,0 +1,69 @@
+// src/stores/itemStore.ts
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { ItemOption } from "@/components/types"; // Assuming you have this type defined
+
+export const useItemStore = defineStore("itemStore", () => {
+  const itemOptions = ref<ItemOption[]>([]); // All available ingredients
+  const selectedItems = ref<{
+    [id: string]: { quantity: number };
+  }>({}); // Selected ingredients with quantities
+
+  // Computed property to categorize items
+  const ingredientsByCategory = computed(() => {
+    const categories: { [key: string]: ItemOption[] } = {
+      Base: [],
+      Premium: [],
+      Dressing: [],
+      Topping: [],
+    };
+
+    itemOptions.value.forEach((ingredient) => {
+      if (categories[ingredient.classification]) {
+        categories[ingredient.classification].push(ingredient);
+      }
+    });
+
+    return categories;
+  });
+
+  // Add an item or increment its quantity
+  const addItem = (itemId: string) => {
+    if (selectedItems.value[itemId]) {
+      selectedItems.value[itemId].quantity += 1;
+    } else {
+      selectedItems.value[itemId] = { quantity: 1 };
+    }
+  };
+
+  // Remove an item or decrement its quantity
+  const removeItem = (itemId: string) => {
+    if (selectedItems.value[itemId]) {
+      if (selectedItems.value[itemId].quantity > 1) {
+        selectedItems.value[itemId].quantity -= 1;
+      } else {
+        delete selectedItems.value[itemId]; // Remove the item if quantity reaches 0
+      }
+    }
+  };
+
+  // Check if an item is selected and get its quantity
+  const getItemQuantity = (itemId: string) => {
+    return selectedItems.value[itemId]?.quantity || 0;
+  };
+
+  // Check if an item is selected and get its quantity
+  const isItemSelected = (itemId: string) => {
+    return selectedItems.value[itemId]?.quantity || 0 == 0;
+  };
+
+  return {
+    itemOptions,
+    selectedItems,
+    ingredientsByCategory,
+    isItemSelected,
+    addItem,
+    removeItem,
+    getItemQuantity,
+  };
+});
